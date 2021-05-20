e15b24c6f334ceb4d949ed50ee5e24b0300d4131
JD-Wang <>
2021-05-20 18:01:57 +0800

    feat: 使用nginx的跨域配置

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const fs = require('fs')
 const app = new Koa()
 const { port } = require('./package.json')
 
-//配置 cors 的中间件 
-app.use(async (ctx, next)=> {
-  ctx.set('Access-Control-Allow-Origin', '*');
-  ctx.set('Access-Control-Max-Age', 600);
-  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  if (ctx.method == 'OPTIONS') {
-    ctx.body = 200; 
-  } else {
-    await next();
-  }
-});
+//配置 cors 的中间件 , nginx 配置就可以
+// app.use(async (ctx, next)=> {
+//   ctx.set('Access-Control-Allow-Origin', '*');
+//   ctx.set('Access-Control-Max-Age', 600);
+//   ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+//   ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//   if (ctx.method == 'OPTIONS') {
+//     ctx.body = 200; 
+//   } else {
+//     await next();
+//   }
+// });
 // app.use(
 //   cors({
 //       origin: async function (ctx, next) { //设置允许来自指定域名请求
